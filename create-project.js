1569cc852f85041a7081e2274cd4006ebdc9cfbf
Michael Feldstein <msfeldstein@gmail.com>
2017-01-09 22:08:44 -0800

    Make it work on windows

--- create-project.js
+++ create-project.js
@@ -1,35 +1,38 @@
 var fsx = require('fs-extra')
 var path = require('path')
 var exec = require('child_process').exec
+function main() {
+  var name = process.argv[2]
+  if (!name) {
+    console.error("Missing a project name")
+    console.error("$ vive-boilerplate name-of-new-project")
+    return
+  }
 
-var name = process.argv[2]
-if (!name) {
-  console.error("Missing a project name")
-  console.error("$ vive-boilerplate name-of-new-project")
-  return
+  var src = __filename
+  src = src.substring(0, Math.max(src.lastIndexOf("/"), src.lastIndexOf("\\")))
+  var dest = path.join(process.cwd(), name)
+
+  fsx.mkdirSync(dest)
+  process.chdir(dest)
+
+  fsx.copySync(
+    path.join(src, "package.json"),
+    "package.json"
+  )
+
+  fsx.copySync(
+    path.join(src, "src"),
+    "src"
+  )
+
+  console.log("Project created, installing dependencies...")
+  var child = exec("npm install")
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+  child.on('close', function() {
+    console.log("\n\nYou're all set!  Just run 'npm start' from the project directory\n\n")
+  })
 }
 
-var src = __filename
-src = src.substring(0, Math.max(src.lastIndexOf("/"), src.lastIndexOf("\\")))
-var dest = path.join(process.cwd(), name)
-
-fsx.mkdirSync(dest)
-process.chdir(dest)
-
-fsx.copySync(
-  path.join(src, "package.json"),
-  "package.json"
-)
-
-fsx.copySync(
-  path.join(src, "src"),
-  "src"
-)
-
-console.log("Project created, installing dependencies...")
-var child = exec("npm install")
-child.stdout.pipe(process.stdout)
-child.stderr.pipe(process.stderr)
-child.on('close', function() {
-  console.log("\n\nYou're all set!  Just run 'npm start' from the project directory\n\n")
-})
+main()
